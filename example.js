76f956d8e543f428139b10b7ea03b2aa7c9159ba
dignifiedquire <dignifiedquire@gmail.com>
2015-10-16 15:22:12 +0200

    Remove test example

--- example.js
+++
@@ -1,9 +0,0 @@
-// subcomandante
-var run = require('./index')
-var res = run(['ls', '/']) // will exit with parent.
-
-res.pipe(process.stdout)
-
-setTimeout(function () {
-  process.exit(0)
-}, 200)
